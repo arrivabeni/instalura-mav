c0d13a623a397b89d95ab25ae69f9f3186213fd4
Marcelo Vieira <marcelo@informatico.com.br>
2021-03-14 08:27:21 -0300

    feat: final aula 01 módulo 3

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Logo } from '../../../theme/Logo';
 import Text from '../../foundation/Text';
 import { Button } from '../Button';
@@ -16,11 +17,11 @@ const links = [
   },
   {
     texto: 'Sobre',
-    url: '/sobre',
+    url: '/about',
   },
 ];
 
-export default function Menu() {
+export default function Menu({ onCadastrarClick }) {
   return (
     <MenuWrapper>
       <MenuWrapper.LeftSide>
@@ -38,13 +39,17 @@ export default function Menu() {
         })}
       </MenuWrapper.CentralSide>
       <MenuWrapper.RightSide>
-        <Button ghost variant="secondary.main">
+        <Button ghost variant="secondary.main" href="/app/login">
           Entrar
         </Button>
-        <Button variant="primary.main">
+        <Button variant="primary.main" onClick={onCadastrarClick}>
           Cadastrar
         </Button>
       </MenuWrapper.RightSide>
     </MenuWrapper>
   );
 }
+
+Menu.propTypes = {
+  onCadastrarClick: PropTypes.func.isRequired,
+};
